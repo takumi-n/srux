746a257533109a2a7b9e7809f104a8956a6fc6d5
takumi-n <ca01068@cartahd.com>
2023-02-16 12:58:25 +0900

    🚀

---
+++ README.md
@@ -0,0 +1,43 @@
+# Srux
+
+Srux is a chatbot that allows you to see CrUX data for your site from Slack
+
+## Usage
+
+```
+srux <metric> [url]
+```
+
+|arg|description|
+|-|-|
+|`<metric>`|Required. LCP or CLS or FID or TTFB or INP or FCP|
+|`[url]`|Optional. URL of the page where you want to see CrUX data. If omitted, data for the entire origin|
+
+### Examples
+
+```
+srux LCP https://example.com/hello
+```
+
+```
+srux FID
+```
+
+## Installation
+
+### Environment Variables
+
+|Key|Value|
+|-|-|
+|SLACK_BOT_TOKEN|[Slack bot token](https://slack.dev/bolt-js/tutorial/getting-started#setting-up-your-project)|
+|SLACK_SIGNING_SECRET|[Slack signing secret](https://slack.dev/bolt-js/tutorial/getting-started#setting-up-your-project)|
+|CRUX_API_KEY|[CrUX API key](https://developer.chrome.com/docs/crux/api/#crux-api-key)|
+|ORIGIN|Your site origin like `https://example.com`|
+|SITE_NAME|Your site name|
+
+### Start
+
+```sh
+$ yarn install
+$ yarn start
+```
---
+++ app.js
@@ -0,0 +1,83 @@
+const assert = require('assert');
+const { App } = require('@slack/bolt');
+const axios = require('axios').default;
+
+assert(process.env.SLACK_BOT_TOKEN);
+assert(process.env.SLACK_SIGNING_SECRET);
+assert(process.env.CRUX_API_KEY);
+assert(process.env.ORIGIN);
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+const metricsMap = {
+  LCP: 'largest_contentful_paint',
+  CLS: 'cumulative_layout_shift',
+  FID: 'first_input_delay',
+  TTFB: 'experimental_time_to_first_byte',
+  FCP: 'first_contentful_paint',
+  INP: 'experimental_interaction_to_next_paint',
+};
+
+app.message(/^srux/, async ({ message, say }) => {
+  const [dirtyAcronym, url] = message.text.split(' ').slice(1);
+  if (!dirtyAcronym) {
+    return;
+  }
+
+  const acronym = dirtyAcronym.toUpperCase();
+
+  const metric = metricsMap[acronym];
+  if (!metric) {
+    return;
+  }
+
+  const sanitizedUrl = url ? url.replace('<', '').replace('>', '') : '';
+  const params = sanitizedUrl ? { url: sanitizedUrl } : { origin: process.env.ORIGIN };
+  const resp = await axios.post(
+    `https://chromeuxreport.googleapis.com/v1/records:queryHistoryRecord?key=${process.env.CRUX_API_KEY}`,
+    {
+      ...params,
+      formFactor: 'PHONE',
+      metrics: [metric],
+    }
+  );
+
+  const graphData = {
+    type: 'line',
+    data: {
+      labels: resp.data.record.collectionPeriods.map((p) => `${p.lastDate.month}/${p.lastDate.day}`),
+      datasets: [
+        {
+          label: acronym,
+          data: resp.data.record.metrics[metric].percentilesTimeseries.p75s,
+          fill: false,
+          borderColor: 'blue',
+        },
+      ],
+    },
+  };
+
+  await say({
+    text: `*${sanitizedUrl || process.env.SITE_NAME || process.env.ORIGIN} ${acronym}*`,
+    attachments: [
+      {
+        color: '#81848f',
+        blocks: [
+          {
+            type: 'image',
+            image_url: `https://quickchart.io/chart?c=${encodeURIComponent(JSON.stringify(graphData))}`,
+            alt_text: `${acronym}`,
+          },
+        ],
+      },
+    ],
+  });
+});
+
+(async () => {
+  await app.start(process.env.PORT || 3000);
+  console.log('⚡️ Bolt app is running!');
+})();
